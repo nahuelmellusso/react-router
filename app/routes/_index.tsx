fdd10d92f04a37ad5c7a4cd38a3534ea28f738d4
nahuel <nahuelmellusso@gmail.com>
2026-02-06 11:55:50 -0300

    add locales and fix index route

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,8 @@
-// app/routes/_index.tsx
 import { redirect } from "react-router";
 
 export async function loader() {
-    // Podés detectar el idioma del navegador aquí si querés.
-    throw redirect("/en/auth/login");
+  throw redirect("/en/auth/login");
 }
-export default function RootIndex() { return null; }
+export default function RootIndex() {
+  return null;
+}
