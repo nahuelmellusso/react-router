47ef06b56dd2b051fcb5fdb85e198ad2cb91e08c
nahuel <nahuelmellusso@gmail.com>
2026-03-10 10:33:44 -0300

    update components index

--- app/components/index.tsx
+++ app/components/index.tsx
@@ -5,4 +5,20 @@ import FormError from "~/components/error/FormError";
 import Table from "~/components/table/Table";
 import Card from "~/components/card/Card";
 import { Drawer } from "~/components/drawer/Drawer";
-export { Input, Button, Spinner, FormError, Table, Card, Drawer };
+import { Select } from "~/components/select/Select";
+import { FormField } from "~/components/formField/FormField";
+import { AvatarPicker } from "~/components/avatar/AvatarPicker";
+import Checkbox from "~/components/checkbox/Checkbox";
+export {
+  Input,
+  Button,
+  Spinner,
+  FormError,
+  Table,
+  Card,
+  Drawer,
+  Select,
+  FormField,
+  AvatarPicker,
+  Checkbox,
+};
